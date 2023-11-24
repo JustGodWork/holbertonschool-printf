58876dfea98e7b4644d3dd3d86412013dbed0331
Adolberton <7557@holbertonstudents.com>
2023-11-24 15:48:53 +0100

    better handling of numbers

--- tests/main.c
+++ tests/main.c
@@ -1,52 +1,47 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * main - Entry point
+ *
  * Return: Always 0
  */
 int main(void)
 {
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
-	int lenD1, lenC1, lenD2, lenC2, lenD3, lenC3, lenD4, lenC4, lenD5, lenC5;
-
-	printf("-----------------\n");
-
-	lenD1 = printf("Let's try to printf a simple sentence.\n");
-	lenC1 = _printf("Let's try to printf a simple sentence.\n");
-
-	printf("-----------------\n");
-
-	/* Replacing %c by 'l' */
-	lenD2 = printf("Hel%co\n", 'l');
-	lenC2 = _printf("Hel%co\n", 'l');
-
-	printf("-----------------\n");
-
-	/* Replacing %s by "World" */
-	lenD3 = printf("Hello %s\n", "World");
-	lenC3 = _printf("Hello %s\n", "World");
-
-	printf("-----------------\n");
-
-	/* Replacing %s by "(null)" */
-	lenD4 = printf("Hello %s\n", NULL);
-	lenC4 = _printf("Hello %s\n", NULL);
-
-	printf("-----------------\n");
-
-	/* Replacing %% by % */
-	lenD5 = printf("Hello %%\n");
-	lenC5 = _printf("Hello %%\n");
-
-	printf("-----------------\n\n");
-
-	printf("Lengths:\n");
-
-	printf("(Default[1]: %d, Custom[1]: %d)\n", lenD1, lenC1);
-	printf("(Default[2]: %d, Custom[2]: %d)\n", lenD2, lenC2);
-	printf("(Default[3]: %d, Custom[3]: %d)\n", lenD3, lenC3);
-	printf("(Default[4]: %d, Custom[4]: %d)\n", lenD4, lenC4);
-	printf("(Default[5]: %d, Custom[5]: %d)\n", lenD5, lenC5);
-
-	return (0);
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    printf("debug --------------------------\n");
+     _printf("Length:[%d, %i]\n", NULL,1234567890299999);
+    printf("Length:[%d, %i]\n", 12345678902, 1234567890299999);
+    return (0);
 }
